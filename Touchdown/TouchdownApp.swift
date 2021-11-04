dd52124b09a2f77970c53853f6b4407549872d08
YukiseKimi <mannychau@gmail.com>
2021-11-04 19:46:58 +0000

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Manny Chau on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
