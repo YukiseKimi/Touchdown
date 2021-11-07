21c078909a7368e165ebd20d5f456740674ce0d7
YukiseKimi <mannychau@gmail.com>
2021-11-07 23:01:55 +0000

    Custom Nav Bar View with Animation

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Manny Chau on 2021-11-06.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGrey: Color = Color(UIColor.systemGray4)
+// Layout
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
