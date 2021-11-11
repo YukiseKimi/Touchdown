9250b153b1cf0884ad3561dc284cb67cd36e4a1b
YukiseKimi <mannychau@gmail.com>
2021-11-10 21:35:28 -0500

    Player Model

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Manny Chau on 2021-11-10.
+//
+
+import Foundation
+import SwiftUI
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
+
